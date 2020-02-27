4ca361750f8546ff2e6774c7db683e57bc95db60
Steven Wang <stevenwang2060@gmail.com>
2020-02-27 14:59:42 -0800

    Finished with stretch

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import FormikOnboard from './components/Form';
+
+function App() {
+  return (
+    <div className = "App">
+      <FormikOnboard />
+    </div>
+  );
+}
+
+export default App;
